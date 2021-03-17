048701c54c3f249306c1c349bb4d17575d44f613
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-17 00:11:44 -0500

    LeetCode Practice

--- leetcode.js
+++ leetcode.js
@@ -1,3 +1,16 @@
+// Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+//
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+
+// function removeDuplicates(nums) {
+//     let newArr = [...new Set(nums)]
+//     return newArr.length
+// };
+//
+// console.log(removeDuplicates([0,0,1,1,1,2,2,3,3,4]))
+
+
 // Given an input string s, reverse the order of the words.
 //
 // A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
@@ -6,7 +19,7 @@
 //
 // Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
 
-// 
+//
 // function reverseWords(str){
 //   return str.trim().split(' ').reverse().join(' ').replace(/\s+/g, ' ')
 // }
