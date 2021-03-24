a17e205ad05f7a4ddd9444e43ddc3ae4fa671212
Guillermo Aguilar <gaguilar128@gmail.com>
2021-03-24 00:38:04 -0500

    LeetCode Practice

--- leetcode.js
+++ leetcode.js
@@ -1,3 +1,34 @@
+
+
+
+// Write a function that makes this string uppercase and sorts it in alphabetical order by last name.When the last names are the same, sort them by first name. Last name and first name of a guest come in the result between parentheses separated by a comma.
+
+// function meeting(str){
+//   let newArr = []
+//   let newStr = str.toUpperCase().replace(/([\w]+):([\w]+)/g, '$2 $1').split(';').sort((a, b) => {return a > b ? 1 : -1});
+//
+//   for(let i = 0; i < newStr.length; i++){
+//     newArr.push('(' + newStr[i].replace(' ', ', ') + ')')
+//   }
+//   return newArr.join('')
+// }
+//
+//
+// console.log(meeting("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"))
+
+// should return:
+// "(CORWILL, ALFRED)(CORWILL, FRED)(CORWILL, RAPHAEL)(CORWILL, WILFRED)(TORNBULL, BARNEY)(TORNBULL, BETTY)(TORNBULL, BJON)"
+
+// Better version
+// const meeting = s => {
+//   return s.toUpperCase()
+//          .replace(/(\w+):(\w+)/g, "($2, $1)")
+//          .split(';')
+//          .sort()
+//          .join('')
+// };
+//
+
 // Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
 //
 // Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
