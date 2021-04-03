ecd0d8b5132b3e53b0beafc728807b710aad06d3
Guillermo Aguilar <gaguilar128@gmail.com>
2021-04-03 12:41:03 -0500

    LeetCode Practice

--- leetcode.js
+++ leetcode.js
@@ -1,4 +1,19 @@
 
+
+// Write a function that returns a list of strings in an array in reverse order.Ex: '123'   ==>  ['321', '21', '1']
+
+// function reverse(str){
+//   let arr = []
+//   let newStr = str.split('').reverse().join('')
+//   for(let i = 0; i < newStr.length; i++){
+//     arr.push(str.slice(0+i))
+//   }
+//   return arr
+// }
+//
+// console.log(reverse('123'))
+
+
 //Write a recursive function called replicate which will receive arguments times and number.The function should return an array containing repetitions of the number argument. For instance, replicate(3, 5) should return [5,5,5]. If the times argument is negative, return an empty array.
 
 
